cf192a8cc19431a4ce486e52ed8db995539dd660
Lu Xianfan <62139687+FlyyingPiggy2020@users.noreply.github.com>
2025-05-20 15:30:15 +0800

    增加verbose选项，可以看到哪些文件被执行了clang-format

--- format_all.sh
+++ format_all.sh
@@ -1,22 +1,30 @@
 #!/bin/bash  
 # TODO:在DIRS里增加你要格式化代码的文件夹名称例如
-# DIRS=("./Software/Project/Protocol/" "./Software/Project/Source/")
+
+VERBOSE=false
+DIRS=("./board" "./protocol" "./fp-sdk" "./bsp" "./source")
+
+while getopts "v" opt; do
+  case $opt in
+    v) VERBOSE=true ;;
+    *) echo "Usage: $0 [-v]" >&2; exit 1 ;;
+  esac
+done
 
 # 尝试执行Windows特有的命令，用于判断当前的OS类型 
-if command -v cmd >/dev/null 2>&1; then  
-    # windows下使用tools目录下的clang-format.exe格式化代码
-    for dir in "${DIRS[@]}"; do  
-        find "$dir" -type f \( -name "*.c" -o -name "*.h" \) -exec tools/clang-format/clang-format -i {} +  
-    done 
+if command -v cmd >/dev/null 2>&1; then
+  for dir in "${DIRS[@]}"; do
+    $VERBOSE && echo "Processing $dir:"
+    $VERBOSE && find "$dir" -type f \( -name "*.c" -o -name "*.h" \) -print 
+    find "$dir" -type f \( -name "*.c" -o -name "*.h" \) -exec tools/clang-format/clang-format -i {} +
+  done
 else
-    # linux下使用自带的clang-format格式化代码
-    if [ -f /proc/version ]; then  
-        for dir in "${DIRS[@]}"; do  
-            find "$dir" -type f \( -name "*.c" -o -name "*.h" \) -exec clang-format -i {} +  
-        done  
-    else
-    # unknow system
-        echo "Unable to determine the operating system environment."  
-    fi  
+  if [ -f /proc/version ]; then
+    for dir in "${DIRS[@]}"; do
+      $VERBOSE && echo "Processing $dir:"
+      find "$dir" -type f \( -name "*.c" -o -name "*.h" \) -print
+      find "$dir" -type f \( -name "*.c" -o -name "*.h" \) -exec clang-format -i {} +
+    done
+  fi
 fi
 
